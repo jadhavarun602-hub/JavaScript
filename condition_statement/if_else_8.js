5c62ad2a3b1cee7f7bd3d48de2e16d9f0c034801
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-11-27 23:47:51 +0530

    if_else_Q8

--- condition_statement/if_else_8.js
+++ condition_statement/if_else_8.js
@@ -1,6 +1,6 @@
 // Check whether a number is divisible by 5 and 11 or not.
 
-const num = 110;
+const num = 55;
 
 if (num % 5 == 0 && num % 11 == 0) {
     console.log(num+ " this number is divisible by 5 and 11");
