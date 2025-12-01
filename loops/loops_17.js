14a788efc6b164e98ec941d195be11a475dd7d59
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-01 22:44:24 +0530

    loops_Q17

--- loops/loops_17.js
+++ loops/loops_17.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+// Generate a reverse pyramid pattern:
+
+
+let num = 5;
+
+for (let i = 1; i <= num; i++) {
+    let str = "";
+    for (let j = num; j >= i; j--) {
+       str += "*";
+       
+    }
+
+    console.log(str);
+    
+    
+}
