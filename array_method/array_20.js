98d9ce0ab5e8bc25c355693d709ce9f83de9c864
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-02 22:59:43 +0530

    array_Q20

---
+++ array_method/array_20.js
@@ -0,0 +1,14 @@
+// Use lastIndexOf() to find the last occurrence of value.
+
+// let str = "hii i am happy i am fine";
+
+// let last = str.lastIndexOf("fine");
+
+// console.log(last);
+
+let num = [10,20,30,40,50,60,70,80];
+
+let check = num.lastIndexOf(40);
+
+console.log(check);
+
