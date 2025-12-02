6e1d4d8493d1a99657c827cf309bce1f73166f75
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-02 19:53:00 +0530

    array_Q1

---
+++ array_method/array_1.js
@@ -0,0 +1,12 @@
+// Basic Array Methods
+// Create an array of 5 fruits and print the first and last element.
+
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+console.log("First element : ",fruits[0]);
+console.log("Last element : ",fruits[fruits.length-1]);
+
+
+
+
+
