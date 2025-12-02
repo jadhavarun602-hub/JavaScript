19e166c700d499ad3f62340d0ef383f79893ba7b
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-02 22:09:36 +0530

    array_Q14

---
+++ array_method/array_14.js
@@ -0,0 +1,11 @@
+// Use filter() to return numbers greater than 50 from an array.
+
+let num = [44,2,10,33,78,98,1,22,43,65];
+
+let greatar = num.filter(function(check){
+
+    return check > 50;
+
+})
+
+console.log(greatar);
