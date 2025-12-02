6d2c42ec1e0fdc9ea45acfa853439f753d13c86a
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-02 19:59:26 +0530

    array_Q2

---
+++ array_method/array_2.js
@@ -0,0 +1,9 @@
+// Add two new fruits to the end of an array using push().
+
+let fruits =["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+fruits.push("berry","chiku");
+
+console.log(fruits);
+
+
