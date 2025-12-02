9dff85564c6e075bea8cf3305b48d376cfccad68
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-02 20:07:55 +0530

    array_Q5

---
+++ array_method/array_5.js
@@ -0,0 +1,7 @@
+// Remove the first element of an array using shift() and print the removed value.
+
+let Name = ["arun","nandu","sandip","tushar","golu","vishal"];
+
+Name.shift();
+
+console.log(Name);
