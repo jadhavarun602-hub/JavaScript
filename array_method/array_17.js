a159b263052aa27427c81d6747cfc8f816bea3a1
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-02 22:33:33 +0530

    array_Q17

---
+++ array_method/array_17.js
@@ -0,0 +1,9 @@
+// Use forEach() to print each element with its index.
+
+let num = [44,2,10,33,78,98,1,22,43,65];
+
+let print = num.forEach(function(ele,ind){
+
+    console.log("Index : ",ind,"Element : ",ele);
+    
+});
