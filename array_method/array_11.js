17f643e0503768bc2685b48176991d9868d6c3be
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-02 20:47:06 +0530

    array_Q11

---
+++ array_method/array_11.js
@@ -0,0 +1,11 @@
+// Intermediate Array Methods
+// Use map() to return the square of each number in an array.
+
+let value = [3, 6,5,33, 4];
+
+let result = value.map(function (value) {
+
+    return value * value;
+});
+
+console.log(result);
