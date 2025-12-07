5342766c85f5dd5e32f2ee1b39f0a912d7212329
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-07 23:53:47 +0530

    array_Q24

---
+++ array_method/array_24.js
@@ -0,0 +1,18 @@
+// Combine two arrays using concat().
+
+// let arr1 = [1,2,3,4,5];
+// let arr2 = [6,7,8,9,10];
+
+// let arr3 = arr1.concat(arr2);
+
+// console.log(arr3);
+
+let Name = "Arun ";
+let greeting = "Good Morning";
+
+let msg = Name.concat(greeting);
+
+console.log("hii",msg);
+
+
+
