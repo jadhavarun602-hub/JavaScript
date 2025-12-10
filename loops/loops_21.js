67c50e9b80fd13e1b7fe532c21cac261933443c5
jadhavarun602-hub <jadhavarun602@gmail.com>
2025-12-10 21:49:28 +0530

    loops_Q21

---
+++ loops/loops_21.js
@@ -0,0 +1,15 @@
+// Print all elements of an array using a loop.
+
+let arr = [22,44,5,87,90,2,4,7,8];
+
+// for (let i = 0; i < arr.length; i++) {
+   
+//     console.log(arr[i]);
+    
+    
+// }
+
+for (const num of arr) {
+    console.log(num);
+    
+}
