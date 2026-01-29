5870e5a966a94c67516fbca267e750179503daff
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-29 22:39:33 +0530

    Use reduce() to find the largest number in an array

--- array/array_16.js
+++ array/array_16.js
@@ -1,6 +1,6 @@
 // Use reduce() to find the largest number in an array.
 
-let num = [33,55,22,88,73,89,76,32,54];
+let num = [33,55,22,88,73,89,12,76,32,54];
 
 let find = num.reduce(function(num1 ,num2){
     return num1 > num2 ? num1 : num2;
