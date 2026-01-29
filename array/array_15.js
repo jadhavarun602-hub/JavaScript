1e918d66320cad0c001596f17a015b6041179bba
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-29 22:38:24 +0530

    Use reduce() to find the sum of all numbers in an array

--- array/array_15.js
+++ array/array_15.js
@@ -1,6 +1,6 @@
 // Use reduce() to find the sum of all numbers in an array.
 
-let num = [44,2,10,33,78,98,1,22,43,65];
+let num = [44,2,10,33,78,98,,34,1,22,43,65];
 
 let sum = num.reduce(function(add,item){
     return add += item;
