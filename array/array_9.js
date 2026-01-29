a4af86edb0184fab7501f41865568ccd40464091
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-29 22:27:03 +0530

    Sort an array of numbers ascending using sort()

--- array/array_9.js
+++ array/array_9.js
@@ -1,6 +1,6 @@
 // Sort an array of numbers ascending using sort().
 
-let num = [33,66,88,33,11,99,55,64,86,2,34,];
+let num = [33,66,88,33,11,99,55,64,86,,34,2,34,];
 
 num.sort(function(a,b){
     return a -b;
