5394afeebd147249a222435998ce51f2cac24197
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-29 22:54:35 +0530

    Find the index of the first number greater than 20 using findIndex().

--- array/array_29.js
+++ array/array_29.js
@@ -1,6 +1,6 @@
 // Find the index of the first number greater than 20 using findIndex().
 
-let num = [2,8,3,21,1,13,77,5,43,11];
+let num = [2,8,3,21,1,13,5,43,11];
 
 let arr = num.findIndex(n => n > 20);
 
