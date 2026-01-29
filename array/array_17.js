d86059f012fd04660be3aa44f5c6eaeaec6d10f6
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-29 22:40:40 +0530

    Use forEach() to print each element with its index.

--- array/array_17.js
+++ array/array_17.js
@@ -1,6 +1,6 @@
 // Use forEach() to print each element with its index.
 
-let num = [44,2,10,33,78,98,1,22,43,65];
+let num = [44,2,10,33,78,98,1,11,22,43,65];
 
 let print = num.forEach(function(ele,ind){
 
