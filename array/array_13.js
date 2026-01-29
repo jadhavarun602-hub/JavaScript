329c6e52faa0f0093f518adc6f0452d285459e8d
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-29 22:36:36 +0530

    Use filter() to return only even numbers from an array.

--- array/array_13.js
+++ array/array_13.js
@@ -1,6 +1,6 @@
 // Use filter() to return only even numbers from an array.
 
-let number = [2,55,44,42,77,87,48,26,15,62];
+let number = [2,55,44,42,77,87,,43,48,26,15,62];
 
 let evenNum = number.filter(function(num){
 
