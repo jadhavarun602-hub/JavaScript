240f6319989fea7a13bb4b33c1ccfcf73efe66f2
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-29 22:55:29 +0530

    Given an array of objects with name and age, filter only adults (age  18)

--- array/array_30.js
+++ array/array_30.js
@@ -1,6 +1,6 @@
 // Given an array of objects with name and age, filter only adults (age ≥ 18).
 
-let ages = [13,33,21,14,19,24];
+let ages = [13,33,21,55,14,19,24];
 
 let age = ages.filter(n => n >= 18);
 
