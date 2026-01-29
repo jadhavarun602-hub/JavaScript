2e7af39f43c2b93d82cff392c5429317cfad2349
jadhavarun602-hub <jadhavarun602@gmail.com>
2026-01-29 22:41:51 +0530

    Use includes() to check if a number exists in an array

--- array/array_18.js
+++ array/array_18.js
@@ -1,6 +1,6 @@
 // Use includes() to check if a number exists in an array.
 
-let number = [55,44,22,98,23,18,38,87,10];
+let number = [55,44,22,98,2,3,18,38,87,100];
 
 let check = number.includes(100);
 
